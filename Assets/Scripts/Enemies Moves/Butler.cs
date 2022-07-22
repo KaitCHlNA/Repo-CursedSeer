bb2d925d05c6df69e842099073893e995726a45d
KaitCHlNA <kaits.orrego@gmail.com>
2022-07-22 04:45:11 -0400

    v0.0.3

---
+++ Assets/Scripts/Enemies Moves/Butler.cs
@@ -0,0 +1,65 @@
+using Cinemachine;
+using UnityEngine;
+
+public class Butler : MonoBehaviour
+{
+    private Animator anim;
+
+    [SerializeField] private Transform playerPosition;
+    private float speed = 0.1f;
+    private float distance = 2f;
+
+    //private Player _player;
+
+    void Start()
+    {
+       anim = GetComponent<Animator>();
+       playerPosition = GetComponent<Transform>();
+      
+    }
+
+    void Update()
+    {
+        Following();
+    }
+
+    void SetPlayerTransform(Transform player)
+    {
+        playerPosition = player;
+    }
+
+    void Following()
+    {
+        transform.LookAt(playerPosition);
+        transform.position = Vector3.Lerp(transform.position, playerPosition.position, speed * Time.deltaTime);
+
+        EnemyPlayerDistance();
+        ButlerAnim();
+    }
+
+    void EnemyPlayerDistance()
+    {
+        distance = Vector3.Distance(transform.position, playerPosition.position);
+        if (distance <= 6)
+        {
+            speed = 0f;
+        }
+        else
+        {
+            speed = 0.1f;
+        }
+    }
+
+    void ButlerAnim()
+    {
+        if (transform.position != Vector3.zero && distance >= 6 )
+        {
+            anim.SetBool("walking", true);
+        }
+        else 
+        {
+            anim.SetBool("walking", false);
+        }
+    }
+    
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,25 @@ public class Player : MonoBehaviour
 {
     private float hor;
     private float ver;
-    private float moveSpeed = 5f;
+    private float moveSpeed = 3f;
     private float runSpeed = 10f;
-    private float turnSpeed = 250f;
+    private float mouseSensibility = 150f;
+    private float xRotation;
+    private float yRotation;
+    public Transform cam;
     private Vector3 inputPlayer;
-    private Vector3 turnPlayer;
     private Rigidbody rb;
-    private Animator anim;
-
+   
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Movement();
+        MouseCam();
     }
 
     void Movement()
@@ -28,30 +30,30 @@ public class Player : MonoBehaviour
         ver = Input.GetAxis("Vertical");
         hor = Input.GetAxis("Horizontal");
 
-        inputPlayer = new Vector3(0, 0, ver);
-        turnPlayer = new Vector3(0, hor, 0);
+        inputPlayer = new Vector3(hor, 0, ver);
 
-        //rb.AddForce(inputPlayer * moveSpeed);
-        transform.Translate(inputPlayer * moveSpeed * Time.deltaTime);
-        transform.Rotate(turnPlayer * turnSpeed * Time.deltaTime);
-
-        if (inputPlayer != Vector3.zero)
-        {
-            anim.SetBool("isWalking", true);
-        }
-        else
-        {
-            anim.SetBool("isWalking", false);
-        }
+        rb.velocity = inputPlayer;
+        // transform.Translate(inputPlayer * moveSpeed * Time.deltaTime);
+        
         
         float run = Input.GetAxis("Run");
         
         if (run > 0)
         {
-            transform.Translate(inputPlayer * runSpeed * Time.deltaTime);
+            rb.AddForce(inputPlayer * runSpeed);
         }
     }
 
-    
+    void MouseCam()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensibility * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensibility * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -70,70);
+
+        yRotation += mouseX;
+        cam.localRotation= Quaternion.Euler(xRotation,yRotation,0);
+    }
 
 }
